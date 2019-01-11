4c1622c4aab77e0c8434ab225d625ee9eabe853a
fenix <a.sydorenko87@gmail.com>
2019-01-11 19:52:38 +0200

    #feature #fix ([lib/*], test/index.js, package.json) add RangeSearch class, cornerstone fixes, version update

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
 'use strict';
 
-module.exports = binarySearch
+const binarySearch = require('./lib/binarySearch')
+const RangeSearch = require('./lib/rangeSearch')
 
-/**
- * @function binarySearch
- * @description finding integers in a sorted array
- * @example - binarySearch(array, elem)
- * @param {object} array - target array
- * @param {number} elem - target elem
- * @param {object} marker - object with min and max search range params (non required)
- * @returns {number|null} - index of the equal value (if it exists) or null
- * */
-
-function binarySearch (array, elem, marker = {}) {
-	marker.min = marker.min || 0
-	marker.max = marker.max || Math.floor(array.length / 2)
-
-	const left = array[marker.min]
-	const right = array[marker.max]
-	const diff = marker.max - marker.min
-
-	if (elem === right) {
-		return marker.max
-	}
-
-	if (elem === left) {
-		return marker.min
-	}
-  
-	if (diff === 1) {
-		return null
-	}
-  
-	if (elem > right) {
-		marker.min = marker.max
-		marker.max += Math.ceil(diff / 2)
-		return binarySearch(array, elem, marker)
-	}
-
-	if (elem < left) {
-		marker.max = marker.min
-		marker.min -= Math.floor(diff / 2)
-		return binarySearch(array, elem, marker)
-	}
-
-	marker.max -= Math.ceil(diff / 2)
-	return binarySearch(array, elem, marker)
+module.exports = {
+  binarySearch,
+  RangeSearch
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const binarySearch = require('../index')
+const binarySearch = require('../lib/binarySearch')
+const RangeSearch = require('../lib/rangeSearch')
 const Assert = require('assert')
 const arr = [1, 3, 5, 7, 9, 11, 33, 77]
 const bad = [0, 2, 4, 6, 8, 14, 256]
@@ -16,6 +17,34 @@ describe('binarySearch()', function () {
   for (let i = 0; i < arr.length; i++) {
     it('should return index', function () {
       Assert.equal(arr.indexOf(arr[i]), binarySearch(arr, arr[i]))
+    })
+  }
+})
+
+describe('RangeSearch()', function () {
+
+  const from = [1, 444, 1998]
+  const to = [34, 800, 2003]
+  const data = [
+    { val: 0, result: false },
+    { val: 1, result: true },
+    { val: 34, result: true },
+    { val: 444, result: true },
+    { val: 800, result: true },
+    { val: 1998, result: true },
+    { val: 2003, result: true },
+    { val: -1, result: false },
+    { val: 35, result: false },
+    { val: 443, result: false },
+    { val: 881, result: false },
+    { val: 1997, result: false },
+    { val: 2004, result: false }
+  ]
+
+  const rs = new RangeSearch(from, to)
+  for (const { val, result } of data) {
+    it(`should return ${ result }`, async function () {
+      Assert.equal(result, await rs.belongs(val))
     })
   }
 })
